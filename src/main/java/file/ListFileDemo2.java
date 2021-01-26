70e119385f06bffd177b1d2bfe35fbb1a3ead387
zhangchengliang_chutian <1513675115@qq.com>
2021-01-26 19:47:18 +0800

    第三天

--- src/main/java/file/ListFileDemo2.java
+++ src/main/java/file/ListFileDemo2.java
@@ -2,13 +2,13 @@ package file;
 
 import java.io.File;
 import java.io.FileFilter;
-
 /**
  * 获取一个目录中满足指定条件的所有子项
  *
  * File[] listFiles(FileFilter filter)
  * 将当前File对象表示的目录中所有符合给定过滤器要求的子项返回。
- * 该方法会将该目录中所有的子项都经过一次给定的过滤器，然后
+ * 该方法会将该目录中所有的子项都经过一次给定的过滤器，然后仅保留
+ * 并返回符合要求的子项。
  */
 public class ListFileDemo2 {
     public static void main(String[] args) {
@@ -23,6 +23,11 @@ public class ListFileDemo2 {
                     return name.contains("e");
                 }
             };
+             /*
+                重载的listFiles方法会将dir中每个子项都经过一次
+                给定的过滤器的accept方法，并最终返回所有返回为
+                true的子项。
+             */
             File[] subs = dir.listFiles(filter);
             System.out.println("子项个数：" + subs.length);
             for (int i=0;i<subs.length;i++){
@@ -31,3 +36,18 @@ public class ListFileDemo2 {
         }
     }
 }
+//class MyFilter implements FileFilter {
+//    /**
+//     * 该方法的作用就是定义参数file是否符合要求，符合则返回true
+//     * @param file
+//     * @return
+//     */
+//    public boolean accept(File file) {
+//        //凡是名字中含有"e"的就接受
+//        String name = file.getName();
+//        System.out.println("正在过滤:"+name);
+//        //判断name中是否含有"e"
+//        boolean contains = name.contains("e");
+//        return contains;
+//    }
+//}
